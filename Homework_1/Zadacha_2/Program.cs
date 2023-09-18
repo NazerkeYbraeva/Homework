f806f8a5db924b0711278b33699a41339a6d0b82
Nazerke <ybraeva.n@gmail.com>
2023-09-18 18:30:16 +0600

    Zadacha 2

---
+++ Homework_1/Zadacha_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое 
+// меньшее.
+
+Console.WriteLine("Input num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input num2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2)
+{
+    Console.WriteLine($"Max number -> {num1}");
+}
+else
+{
+    Console.WriteLine($"Max number -> {num2}");
+}
