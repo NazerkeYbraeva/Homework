94cb18a8b90a1fcd82846782a5b06b78114cf05a
Nazerke <ybraeva.n@gmail.com>
2023-09-18 18:40:06 +0600

    Zadacha 6

---
+++ Homework_1/Zadacha_6/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает 
+//число и выдаёт, является ли число чётным (делится ли 
+//оно на два без остатка).
+
+Console.WriteLine("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
