1c64ae2ddd348cb576af8935c5c518b4213a0abb
Nazerke <ybraeva.n@gmail.com>
2023-09-28 17:29:03 +0600

    zadacha_27

---
+++ Homework_4/Zadacha_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int FindSumDigit(int num)
+{
+    int sum = 0;
+    for (int i = 0; num > 0;) 
+    {
+        i = num % 10;
+        sum += i;
+        num /= 10;
+    }
+    return sum;
+}
+Console.WriteLine("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindSumDigit(num));
