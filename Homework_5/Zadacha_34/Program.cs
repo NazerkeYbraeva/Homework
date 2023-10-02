fe6289eab79573f4a5a1d4bea668169f3d43b1bd
Nazerke <ybraeva.n@gmail.com>
2023-10-02 15:15:40 +0600

    Zadacha 34

---
+++ Homework_5/Zadacha_34/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите 
+// программу, которая покажет количество чётных чисел
+// в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) 
+    {
+        array[i] = new Random().Next(99 + 1, 999);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void CountEven(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    Console.WriteLine($"Количество четных элементов -> {count}");
+}
+
+Console.WriteLine("Input size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int[] myArray = CreateRandomArray(size);
+PrintArray(myArray);
+CountEven(myArray);
