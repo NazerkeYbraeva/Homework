c9dbc01cbde3a736cef359446343a6e670032b22
Nazerke <ybraeva.n@gmail.com>
2023-10-02 18:21:54 +0600

    Zadacha 36

---
+++ Homework_5/Zadacha_36/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, стоящих 
+// на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) 
+    {
+        array[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+    
+void FindSumNotEven(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2) 
+        {
+            sum = sum + array[i];
+        }
+        Console.WriteLine($"Сумма элементов на нечетных позициях -> {sum}");
+}
+
+Console.WriteLine("Input size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+PrintArray(myArray);
+
+FindSumNotEven(myArray);
